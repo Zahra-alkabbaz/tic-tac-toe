970671bafad90192d948ec82917ab43c0846e932
Zahra-alkabbaz <71462997+Zahra-alkabbaz@users.noreply.github.com>
2020-09-18 16:37:18 +0300

    updating the grid functionalities

--- tic-tac-toe.c
+++ tic-tac-toe.c
@@ -8,11 +8,10 @@
 #define false 0
 #define MaxMemory 100
 
-int TotalMoves, grid_size, sequence_number, gridFull, initGrid, PossibleMoves, win_decision;
+int x,y, TotalMoves, grid_size, sequence_number, gridFull, PossibleMoves, win_decision;
 char letter = 'X';
 int successfulMove = 1;
-int y = -1;
-int x = -1;
+
 
 //the maximum size of the grid
 #define MaxGrid 10
@@ -27,16 +26,15 @@ struct Move
 
 char grid[MaxGrid][MaxGrid];
 /*
- * Checks if the inputted gri size is within the range; Returns true if the input is invalid, and false otherwise.
+ * Checks if the given grid size is within the range; Returns true if the input is invalid, and false otherwise.
  */
-int init_grid(int gridsize) {
-	int i, j;
-  grid_size = gridsize; // to use it in the other functions
-  if ( gridsize >= 3 && gridsize <= MaxGrid) // ifthe grid size is within the specified range
-  {
-		TotalMoves =0;
-		for ( i= 0; i < gridsize; i++)
-		    for (j= 0; j < gridsize; j++)
+int init_grid() {
+    int i, j;
+    if ( grid_size >= 3 && grid_size <= MaxGrid) // ifthe grid size is within the specified range
+    {
+        TotalMoves =0;
+		for ( i= 0; i < grid_size; i++)
+		    for (j= 0; j < grid_size; j++)
 				 grid[i][j] = '.'; // intializing all grid values to "."
 		return false;
 	}
@@ -83,23 +81,28 @@ return false;
 }
 
 
- //Returns true if it encountered an error and false otherwise.
+ //Returns false if it encountered an error and true otherwise.
 struct Move AllMoves[MaxMemory];
 int make_move(int x, int y, char letter) {
+
  // to check if this point is not used before and is within the range
   if ((grid[x][y] == '.') && (x >=0 && x< grid_size) && (y>=0 &&y<grid_size))
   {
-		TotalMoves++; // to count the numbers of moves once more move added
-    grid[x][y] = letter; // if not used we insert the letter of the player
-// assigning values to the specific Move in the struct where TotalMove sis a counter for the moves
-		AllMoves[TotalMoves].x = x;
+      // to count the numbers of moves once more move added
+      TotalMoves++;
+
+      // if not used we insert the letter of the player
+      grid[x][y] = letter;
+      
+      // assigning values to the specific Move in the struct where TotalMove sis a counter for the moves
+      AllMoves[TotalMoves].x = x;
 	  AllMoves[TotalMoves].y = y;
 	  AllMoves[TotalMoves].player = letter;
-
-		return false;
+      
+      return true;
 	}
-// if the point is not within the range we return true
-  return true;
+// if the point is not within the range we return false
+  return false;
 }
 
 /* Returns the move performed at the "sequence_number"-th step during the current game, where the
@@ -145,6 +148,8 @@ void game_board(int grid_size) {
 // to check the number of successful Moves
 //int x, int y, char letter
 void player () {
+    int y = -1;
+    int x = -1;
 
 	// if moves counter is odd  then it's player 1 (X)
     if ( successfulMove%2 == 0 ){letter ='O'; }
@@ -155,25 +160,37 @@ void player () {
 	printf("\n\nPlayer %c, enter location (x,y): ", letter);
 	scanf("%i,%i", &x, &y);
 
+    //loops until a valid value is given
+    while ((x == -1) || (y == -1)){
+        //reset valyes to -1 again if both or any is not given
+        x = -1;
+        y = -1;
+        printf("\n\nPlayer %c, enter location in this format x,y: ", letter);
+        scanf("%i,%i", &x, &y);
+        }
 
-	if (grid[x][y] == '.') {
+    int move = make_move( x, y, letter);
+	if (move) {
 		// countung the number of successful Moves
 		successfulMove++;
 		grid[x][y] = letter;
-	}
 
-	// if the point is occupied it asks for another one
-	else if ( grid[x][y] != '.' && (x>=0 && x<MaxGrid))
-	{
-		printf("Point is occupied\n"); 
-		// replay_move();
-		player();
+        printf("%i,%i", x, y);
 	}
 
 	// if the move was not successful it tells the player that they should have another input
 	else
 	{
-		printf("invalid input\n"); 
+        // prints an error message if point is already occupied.
+        if(grid[x][y] != '.' && (x >=0 && x< grid_size) && (y>=0 &&y<grid_size))
+        {
+            printf("\n ******** Invalid choice: point is already occupied. Please try again. ********\n"); 
+        }
+
+        //prints an error message if chosen numbers are out of the range.
+        else {printf("\n******** Invalid input: point is out of range. Please try again. ********\n"); }
+
+        //recalls the function to ask for input
 		player();
 	}
  }
@@ -211,29 +228,26 @@ int main() {
 
 	//creating an instance of the struct.
 	struct Move playerOne, playerTwo;
-
-
 	PossibleMoves = grid_size*grid_size;
-	int Move = make_move( x, y, letter);
 
 	printf("Hello, Here is the Tic-Tac-Toe Game!\n");
 	printf("Please choose a grid size for the Game between 3 and 10 inclusive: "); // asking the user for grid size
 	scanf("%i", &grid_size); // assinging the given the value to a variable for later use
 
-	if (init_grid(grid_size) == true) // if grid size is not within the range it asks for another input
+	if (init_grid() == true) // if grid size is not within the range it asks for another input
 	{
-		printf("grid size invalid try again");
-		printf("Please choose a grid size for the Game between 3 and 10 inclusive: "); // asking the user for grid size
+		printf("\nInvalid grid size: Please choose a grid size for the Game between 3 and 10 inclusive: "); // asking the user for grid size
 		scanf("%i", &grid_size);
 	}
-
-  init_grid(grid_size); // calling the previously written function to use its values
-  char status= 'a';
+    
+    init_grid(); // calling the previously written function to use its values
+    char status= 'a';
 
 	while (status == 'a') // game c
 	{
-		game_board(grid_size); // to start the game
-		// player(x, y, letter);
+         // to start the game
+		game_board(grid_size);
+	
 		player();
 		// player_won(letter);
 		// if (win_decision == true){status = 'b';}
